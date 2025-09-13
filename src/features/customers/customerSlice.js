02f1b446ad081e38e4a2de6dee3ba9cde9bd1088
Robert Young <david41596@yahoo.com>
2025-09-13 15:48:08 -0400

    Converting old redux code to more modern "Redux Tool Kit"

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,27 +1,36 @@
-const initalStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
 
-export default function customerReducer(state = initalStateCustomer, action) {
-  switch (action.type) {
-    case "customer/create":
-      return { ...state, ...action.payload };
-    case "customer/updateName":
-      return { ...state, fullName: action.payload };
-    default:
-      return state;
-  }
-}
+const customerSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
-export function createCustomer(fullName, nationalID) {
-  return {
-    type: "customer/create",
-    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-  };
-}
-
-export function updateName(fullName) {
-  return { type: "customer/updateName", payload: fullName };
-}
+export const { createCustomer, updateName } = customerSlice.actions;
+export default customerSlice.reducer;
